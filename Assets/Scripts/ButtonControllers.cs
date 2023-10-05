2dd9e69cc359bd952da7deea5ed8ef5c4e42d3bd
willpk03 <43060381+willpk03@users.noreply.github.com>
2023-10-05 16:00:56 +1000

    Main menu added
    Main Menu Set up and New Font

---
+++ Assets/Scripts/ButtonControllers.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonControllers : MonoBehaviour
+{
+    public void Scene1() {  
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Scene1");  
+    }  
+    public void Scene2() {  
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Scene2");  
+    }  
+    public void Scene3() {  
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Scene3");  
+    }  
+
+}
